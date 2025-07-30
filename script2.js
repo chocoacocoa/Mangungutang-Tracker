bbba382e6004451ceac785be6e41e2fbb3ac96cf
chocoacocoa <vancholo2006@gmail.com>
2025-07-30 13:03:39 +0800

    functions are done, design nalang

--- script2.js
+++ script2.js
@@ -1,27 +1,31 @@
-let data = JSON.parse(localStorage.getItem("userData")) || {
-  name: [],
-  amount: [],
-  date: [],
-  description: [],
-};
+let data = JSON.parse(localStorage.getItem("userData")) || [
+  {
+    name: "",
+    amount: null,
+    date: "",
+    description: "",
+    id: null,
+  },
+];
+
 localStorage.setItem("userData", JSON.stringify(data));
-console.log(data.name);
-console.log(data.amount[0]);
-console.log(data.date[0]);
-console.log(data.description[0]);
+
+for (let i = 0; i < data.length; i++) {
+  console.log(data[i]);
+}
+
 let main = document.getElementById("main");
 const namer = document.getElementById("namer");
 const amount = document.getElementById("amount");
 const date = document.getElementById("date");
 const description = document.getElementById("description");
+let userID = 0;
 //----------------------------------------------------------------------------
 const addDebt = document.getElementById("addDebt");
 const clear = document.getElementById("clear");
 const deleteData1 = document.getElementById("deleteData");
 //----------------------------------------------------------------------------
 let totalDebt = document.getElementById("totalDebt");
-let edit;
-let paid;
 //----------------------------------------------------------------------------
 let logField = document.getElementById("logField");
 let loggers = document.getElementById("loggers");
@@ -32,40 +36,50 @@ let totalDebtSum = 0;
 let header = document.getElementById("header");
 let aboutSection = document.getElementById("aboutSection");
 let aboutButton = document.getElementById("aboutButton");
-const today = new Date();
-let id = 0;
+let paidVerify = document.getElementById("paidVerify");
+let paidYes = document.getElementById("paidYes");
+let paidNo = document.getElementById("paidNo");
+
 //----------------------------------------------------------------------------
 
-function nyff() {
-  mainNyf.innerHTML = `
-  <div class="nyf">
-             <div>
-                <img src="/media/pic.jpg">
-                <h4>Sorry po, hindi pa po tapos</h4>
-                <button id="nyfY">Oki pow</button>
-            </div>
-        </div>
- 
-`;
-  nyfY = document.getElementById("nyfY");
+//FUNCTIONS----------------------------------------------------------------------------
+
+function totalDebtResponse() {
+  let sum = 0;
+  for (let i = 0; i < data.length; i++) {
+    sum += Number(data[i].amount);
+  }
+  console.log(sum);
+  totalDebt.innerHTML += sum;
 }
 
-function deleteData() {
-  data.name = [];
-  data.amount = [];
-  data.date = [];
-  data.description = [];
+function markAsPaid(id) {
+  const debt = data.find((item) => item.id === id);
+
+  if (confirm(`Mark ${debt.name}'s debt of ₱${debt.amount} as paid?`)) {
+    // Remove from array
+    data = data.filter((item) => item.id !== id);
+
+    // Update storage and UI
+    localStorage.setItem("userData", JSON.stringify(data));
+    setTimeout(() => {
+      location.reload();
+    }, 500);
+
+    // calculateTotalDebt();
+  }
+}
+function setupEventListeners() {
+  document.querySelectorAll(".paid").forEach((button) => {
+    onclick(button, function () {
+      const id = parseInt(this.getAttribute("data-id"));
+      markAsPaid(id);
+    });
+  });
 }
 
 function about() {
   aboutButton.addEventListener("click", function () {
-    // if(aboutSection.style.animation == "aboutMoveO"){
-    //   aboutSection.style.animation -= "aboutMoveO";
-    //   aboutSection.style.animation += "aboutMoveE";
-    // }else{
-    //   aboutSection.style.animation -= "aboutMoveE";
-    //   aboutSection.style.animation += "aboutMoveO";
-    // }
     if (aboutSection.style.left !== "0vw") {
       aboutSection.style.left = "0vw";
     } else {
@@ -73,89 +87,86 @@ function about() {
     }
   });
 }
-about();
 
-function totalDebtSummation() {
-  for (let i = 0; i < data.amount.length; i++) {
-    totalDebtSum += Number(data.amount[i]);
-  }
-  console.log("total: ", totalDebtSum);
+function deleteAll() {
+  onclick(deleteData1, function () {
+    const blankData = [
+      {
+        name: "",
+        amount: null,
+        date: "",
+        description: "",
+        id: null,
+      },
+    ];
+    data = blankData;
+    localStorage.setItem("userData", JSON.stringify(data));
+    setTimeout(() => {
+      location.reload();
+    }, 500);
+  });
+}
+
+function onclick(a, funct) {
+  a.addEventListener("click", funct);
+}
+
+function clearForm() {
+  onclick(clear, function () {
+    namer.value = "";
+    amount.value = null;
+    date.value = "";
+    description.value = "";
+  });
 }
 
 function load() {
-  totalDebtSummation();
-  totalDebt.innerHTML += totalDebtSum;
-
-  for (let i = 0; i < data.name.length; i++) {
-    id++;
+  for (let i = 1; i < data.length; i++) {
     loggers.innerHTML += `
         <fieldset class="logField">
                 <div>
-                <p id="user${id}">ID: ${id}</p>
-                <h5>Name: ${data.name[i]}</h5>
-                <h6 id="borrowed">Utang: <u>₱ ${data.amount[i]}</u></h6>
-                <h6 id="borrowedDate">Date: ${data.date[i]}</h6>
-                <h6 id="ⓘborrowerDescription">Description: ${data.description[i]}</h6>
+                <h5>Name: ${data[i].name}</h5>
+                <h6 id="borrowed">Utang: <u>₱ ${data[i].amount}</u></h6>
+                <h6 id="borrowedDate">Date: ${data[i].date}</h6>
+                <h6 id="ⓘborrowerDescription">Description: ${data[i].description}</h6>
                 </div>
                 <div>
-                <button class="edit">Edit</button>
-                <button class="paid">Paid</button>
+                <button class="paid" data-id="${data[i].id}">Paid</button>
                 </div>
             </fieldset>
             <br>
         `;
-    edit = document.querySelectorAll(".edit");
-    paid = document.querySelectorAll(".paid");
   }
-}
-function save() {
-  localStorage.setItem("userData", JSON.stringify(data));
+  totalDebtResponse();
+  setupEventListeners();
 }
 
+function newUtang() {
+  onclick(addDebt, function () {
+    data = JSON.parse(localStorage.getItem("userData")) || [];
+    const nameValue = namer.value.trim();
+    const amountValue = amount.value.trim();
+    const dateValue = date.value.trim();
+    const descValue = description.value.trim();
+    if (!nameValue || !amountValue || !dateValue) {
+      console.log("Required fields are missing!");
+      return;
+    }
+    const newEntry = {
+      name: nameValue,
+      amount: amountValue,
+      date: dateValue,
+      description: descValue,
+      id: data.length > 0 ? data[data.length - 1].id + 1 : 1,
+    };
+    data.push(newEntry);
+    localStorage.setItem("userData", JSON.stringify(data));
+    console.log("New object added: ", newEntry);
+  });
+}
+//FUNCTIONS----------------------------------------------------------------------------
+about();
+deleteAll();
+clearForm();
 load();
-
-addDebt.addEventListener("click", function () {
-  if (namer.value !== "") {
-    data.name.push(namer.value);
-    data.amount.push(amount.value);
-    data.date.push(date.value);
-    data.description.push(description.value);
-    save();
-    setTimeout(() => {
-      location.reload();
-    }, 100);
-  }
-});
-
-deleteData1.addEventListener("click", function () {
-  deleteData();
-  localStorage.setItem("userData", JSON.stringify(data));
-  alert("data deleted");
-  location.reload();
-});
-
-clear.addEventListener("click", function () {
-  namer.value = "";
-  amount.value = null;
-  date.value = "";
-  description.value = "";
-});
-
-document.addEventListener("click", function (e) {
-  if (e.target && e.target.classList.contains("edit")) {
-    nyff();
-    nyfY.addEventListener("click", function () {
-      location.reload();
-    });
-  }
-  if (e.target && e.target.classList.contains("paid")) {
-    nyff();
-    nyfY.addEventListener("click", function () {
-      location.reload();
-    });
-  }
-});
-
-function editButton(){
-
-}
+newUtang();
